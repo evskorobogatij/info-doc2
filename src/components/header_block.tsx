7516a86b5012e6a22036f97d51dac7eb4bc95a1d
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-09-29 12:40:05 +0300

    Скрытие неактивных разделов в переключателе

--- src/components/header_block.tsx
+++ src/components/header_block.tsx
@@ -5,34 +5,40 @@ import {
   selectRecordMode
 } from '@models/infomat'
 import clsx from 'clsx'
+import { DocumentIcon } from '@icons/document_icon'
+import { UserIcon } from './user_icon'
 
 export const HeaderBlock = () => {
   const mode = useStore($viewMode)
   return (
-    <div className="absolute right-[1px] flex flex-col justify-center items-end bg-transparent h-screen gap-4 z-50 w-36">
+    <div className="absolute right-[1px] flex flex-col justify-center items-end bg-transparent h-screen gap-4 z-50">
       {/* <div className="flex gap-4 bg-white rounded-b-lg h-full p-2">
         <button className="btn btn-primary btn-sm">Запись к врачу</button>
         <button className="btn btn-ghost btn-sm">Документы</button>
       </div> */}
       <div
         className={clsx(
-          'btn rounded-l-lg rounded-r-none w-36',
-          mode === 'record' ? 'btn-primary ' : 'btn-outline btn-ghost bg-white'
+          'btn rounded-l-lg rounded-r-none flex-row ',
+          mode === 'record'
+            ? 'btn-primary '
+            : 'btn-square btn-outline btn-ghost bg-white'
         )}
         onClick={() => selectRecordMode()}
       >
-        Запись к врачу
+        <UserIcon />
+        {mode === 'record' && 'Запись к врачу'}
       </div>
       <div
         className={clsx(
-          'btn rounded-l-lg rounded-r-none w-36',
+          'btn rounded-l-lg rounded-r-none ',
           mode === 'documents'
-            ? 'btn-primary '
-            : 'btn-outline btn-ghost bg-white'
+            ? 'btn-primary'
+            : 'btn-square btn-outline btn-ghost bg-white'
         )}
         onClick={() => selectDocumentMode()}
       >
-        Документы
+        <DocumentIcon />
+        {mode === 'documents' && 'Документы'}
       </div>
     </div>
   )
