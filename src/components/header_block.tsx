ad9f4a3095b3a4debf8b194bba6b4cf90818cb67
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-09-30 14:42:40 +0300

    Изменения вида кнопок переключения разделов

--- src/components/header_block.tsx
+++ src/components/header_block.tsx
@@ -11,7 +11,7 @@ import { UserIcon } from './user_icon'
 export const HeaderBlock = () => {
   const mode = useStore($viewMode)
   return (
-    <div className="absolute right-[1px] flex flex-col justify-center items-end bg-transparent h-screen gap-4 z-50">
+    <div className="absolute right-[1px] flex flex-col justify-center items-end bg-transparent h-screen gap-4 z-50 w-36">
       {/* <div className="flex gap-4 bg-white rounded-b-lg h-full p-2">
         <button className="btn btn-primary btn-sm">Запись к врачу</button>
         <button className="btn btn-ghost btn-sm">Документы</button>
@@ -19,26 +19,24 @@ export const HeaderBlock = () => {
       <div
         className={clsx(
           'btn rounded-l-lg rounded-r-none flex-row ',
-          mode === 'record'
-            ? 'btn-primary '
-            : 'btn-square btn-outline btn-ghost bg-white'
+          mode === 'record' ? 'btn-primary ' : 'btn-outline btn-ghost bg-white'
         )}
         onClick={() => selectRecordMode()}
       >
-        <UserIcon />
-        {mode === 'record' && 'Запись к врачу'}
+        {/* <UserIcon /> */}
+        Запись к врачу
       </div>
       <div
         className={clsx(
-          'btn rounded-l-lg rounded-r-none ',
+          'btn rounded-l-lg rounded-r-none w-36',
           mode === 'documents'
             ? 'btn-primary'
-            : 'btn-square btn-outline btn-ghost bg-white'
+            : 'btn-outline btn-ghost bg-white'
         )}
         onClick={() => selectDocumentMode()}
       >
-        <DocumentIcon />
-        {mode === 'documents' && 'Документы'}
+        {/* <DocumentIcon /> */}
+        Документы
       </div>
     </div>
   )
